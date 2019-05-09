59e75ce1df01ce581f17715ad5911c9a0df4d889
Xiaodong Zou <suugyoutou@r.recruit.co.jp>
2019-05-09 22:17:44 +0900

    333333333

---
+++ firebase-messaging-sw_backup.js
@@ -0,0 +1,118 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDed0wplBQAobaE0qLFcaa3X_XWjCwOgRE",
+  authDomain: "pwa-push-test-33892.firebaseapp.com",
+  databaseURL: "https://pwa-push-test-33892.firebaseio.com",
+  projectId: "pwa-push-test-33892",
+  storageBucket: "pwa-push-test-33892.appspot.com",
+  messagingSenderId: "540875100731",
+  appId: "1:540875100731:web:0a20afa4e5eb652a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+const messaging = firebase.messaging();
+console.log('aabb');
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+
+
+self.addEventListener("push", function(event) {
+  //送られたプッシュ通知の本文を表示
+  if (Notification.permission == "granted") {
+    console.log("Push Notification Recieved", event);
+    event.waitUntil(
+      self.registration
+        .showNotification(event.data.json().notification.title, {
+          body: event.data.json().notification.body
+        })
+        .then(
+          function(showEvent) {},
+          function(error) {
+            console.log(error);
+          }
+        )
+    );
+  }
+});
+
+
+//キャッシュ名。識別用
+var cacheName = "manekineko";
+
+//キャッシュしたいファイルのリストを登録する
+var filesToCache = ["index.html", "main.js", "jquery.js", "style.css"];
+
+//ブラウザにインストールする
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+//
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+//
+self.addEventListener("fetch", function(event) {
+  if (
+    event.request.cache === "only-if-cached" &&
+    event.request.mode !== "same-origin"
+  ) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("push", function(event) {
+  console.log("Push Notification Recieved", event);
+  if (Notification.permission == "granted") {
+    event.waitUntil(
+      self.registration
+        .showNotification("受信しました", {
+          body: "お知らせです。",
+          icon: "iconV2.png"
+        })
+        .then(
+          function(showEvent) {},
+          function(error) {
+            console.log(error);
+          }
+        )
+    );
+  }
+});
+
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow("https://www.jalan.net")
+  );
+});
--- main.js
+++ main.js
@@ -1,24 +1,74 @@
-//台詞リスト
-var serif = new Array();
-serif[1] = "こんにちはにゃん";
-serif[2] = "おはようにゃん";
-serif[3] = "まねきねこ が あらわれた";
-serif[4] = "イメージするのは常に最強の自分にゃ";
-serif[5] = "お腹空いたにゃん";
-serif[6] = "おやすみにゃ";
+const btnSubscribe = document.getElementById('btnSubscribe');
+const btnUnSubscribe = document.getElementById('btnUnSubscribe');
+const textInstanceIdToken = document.getElementById('textInstanceIdToken');
+const sendWebPushArea = document.getElementById('sendWebPushArea');
+const sendWebPush = document.getElementById('sendWebPush');
 
-serif[7] = "クリックされたにゃ";
+// メッセージング オブジェクトの取得
+const messaging = firebase.messaging();
 
-//ランダムに台詞を表示
-function msg() {
-  var a = Math.floor(Math.random() * 6);
-  $(".fukidashi").text(serif[a + 1]);
+// アプリにウェブ認証情報を設定する
+messaging.usePublicVapidKey("BADl04f91UtAWKcxZNliZGTOTLEnCThN-imwGMIvjRSrryQrDFMqaTWeyfDyaX0NU5Krzm2XZI0iB2bl3-4exKM");
+
+// 権限要求
+function requestPermission() {
+    // 通知を受信する権限を要求する
+    messaging.requestPermission().then(function() {
+        // 現在の登録トークンの取得
+        messaging.getToken().then(function(token) {
+            textInstanceIdToken.value = token;
+            btnSubscribe.style.display = 'none';
+            btnUnSubscribe.style.display = 'block';
+            sendWebPushArea.style.display = 'block';
+            sendWebPush.value = 'https://andus.heteml.jp/firebase_cloud_messaging/send.php?id=' + token;
+        }).catch(function(err) {
+            textInstanceIdToken.value = 'トークンの取得に失敗しました（' + err + '）。';
+        });
+    }).catch(function(err) {
+        textInstanceIdToken.value = '通知の許可が得られませんでした（' + err + '）。';
+    });
 }
 
-Notification.requestPermission(function(status) {
-  console.log("通知の許可:", status);
-});
-
-function msg02(message) {
-  $(".fukidashi").text(message);
+// トークン削除
+function deleteToken() {
+    // トークン取得
+    messaging.getToken().then(function(currentToken) {
+        // トークン削除
+        messaging.deleteToken(currentToken).then(function() {
+            textInstanceIdToken.value = 'トークンが削除されました';
+            btnSubscribe.style.display = 'block';
+            btnUnSubscribe.style.display = 'none';
+            sendWebPushArea.style.display = 'none';
+            sendWebPush.value = '';
+        }).catch(function(err) {
+            textInstanceIdToken.value = 'トークンの削除に失敗しました（' + err + '）。';
+        });
+    }).catch(function(err) {
+        textInstanceIdToken.value = 'トークンの取得に失敗しました（' + err + '）。';
+    });
 }
+
+// 初期化
+window.onload = function() {
+    // イベント登録
+    console.log('aaaa')
+    btnSubscribe.onclick = requestPermission;
+    btnUnSubscribe.onclick = deleteToken;
+    // トークン取得
+    messaging.getToken().then(function(currentToken) {
+        if (currentToken) {
+            // 本来ここでサーバにトークン送る処理
+            //sendTokenToServer(currentToken);
+            textInstanceIdToken.value = currentToken;
+            btnSubscribe.style.display = 'none';
+            btnUnSubscribe.style.display = 'block';
+            sendWebPushArea.style.display = 'block';
+            sendWebPush.value = 'https://andus.heteml.jp/firebase_cloud_messaging/send.php?id=' + currentToken;
+        } else {
+            // トークン無い場合
+            textInstanceIdToken.value = '通知の許可をしていません。「通知を許可する」を押してください。';
+        }
+    }).catch(function(err) {
+        textInstanceIdToken.value = 'トークンの取得に失敗しました（' + err + '）。';
+    });
+};
