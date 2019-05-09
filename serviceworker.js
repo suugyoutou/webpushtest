19a4dafbac427d1edc49e91809cd273766749a8b
Xiaodong Zou <suugyoutou@r.recruit.co.jp>
2019-05-09 16:10:45 +0900

    1

--- serviceworker.js
+++ serviceworker.js
@@ -1,103 +1,43 @@
-//キャッシュ名。識別用
-var cacheName = "manekineko";
+//キャッシュ名（今回はmanekineko という固有の名前）
+var cacheName = 'manekineko';
 
-//キャッシュしたいファイルのリストを登録する
-var filesToCache = ["index.html", "main.js", "jquery.js", "style.css"];
+//キャッシュしたいファイルのリスト(今回は3種類。HTML本体、jsファイル、CSSファイルが対象)
+var filesToCache = [
+   'index.html',
+   'jquery.js',
+   'style.css',
+];
 
-//ブラウザにインストールする
-self.addEventListener("install", function(event) {
+//ブラウザへのインストール時にキャシュしたいファイルを追加する
+self.addEventListener('install',function(event){
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function(cache){
       return cache.addAll(filesToCache);
     })
   );
 });
 
-//
-self.addEventListener("activate", function(event) {
+
+//ServiceWorkerが有効になるときcacheNameがちがうキャッシュを削除する
+self.addEventListener('activate',function(event){
   event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(
-        keyList.map(function(key) {
-          if (key !== cacheName) {
-            return caches.delete(key);
-          }
-        })
-      );
+    caches.keys().then(function(keyList){
+      return Promise.all(keyList.map(function(key){
+        if(key !== cacheName){
+          return caches.delete(key);
+        }
+      }));
     })
   );
   return self.clients.claim();
 });
 
-//
-self.addEventListener("fetch", function(event) {
-  if (
-    event.request.cache === "only-if-cached" &&
-    event.request.mode !== "same-origin"
-  ) {
-    return;
-  }
+
+//キャッシュがあれば呼び出し、ない場合ネットワークから取りに行く
+self.addEventListener('fetch',function(event){
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function(response){
       return response || fetch(event.request);
     })
   );
-});
-
-self.addEventListener("push", function(event) {
-  console.log("Push Notification Recieved", event);
-  if (Notification.permission == "granted") {
-    event.waitUntil(
-      self.registration
-        .showNotification("受信しました", {
-          body: "お知らせです。",
-          icon: "iconV2.png"
-        })
-        .then(
-          function(showEvent) {},
-          function(error) {
-            console.log(error);
-          }
-        )
-    );
-  }
-});
-
-self.addEventListener("notificationclick", function(event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow("https://watanabe0601.github.io/sw.github.io/02/")
-  );
-});
-
-importScripts("/__/firebase/4.10.0/firebase-app.js");
-importScripts("/__/firebase/4.10.0/firebase-messaging.js");
-importScripts("/__/firebase/init.js");
-
-var messaging = firebase.messaging();
-
-importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js");
-
-firebase.initializeApp({
-  messagingSenderId: "<送信者ID>"
-});
-
-self.addEventListener("push", function(event) {
-  //送られたプッシュ通知の本文を表示
-  if (Notification.permission == "granted") {
-    console.log("Push Notification Recieved", event);
-    event.waitUntil(
-      self.registration
-        .showNotification(event.data.json().notification.title, {
-          body: event.data.json().notification.body
-        })
-        .then(
-          function(showEvent) {},
-          function(error) {
-            console.log(error);
-          }
-        )
-    );
-  }
 });
