518219cce58e6585f561085891a7cca8f5cffbf3
Xiaodong Zou <suugyoutou@r.recruit.co.jp>
2019-05-09 21:52:24 +0900

    1

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,17 +1,21 @@
-importScripts("/__/firebase/4.10.0/firebase-app.js");
-importScripts("/__/firebase/4.10.0/firebase-messaging.js");
-importScripts("/__/firebase/init.js");
-
-var messaging = firebase.messaging();
-
-importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js");
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 
+const messaging = firebase.messaging();
 
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
 firebase.initializeApp({
-  messagingSenderId: "540875100731"
+  'messagingSenderId': "540875100731"
 });
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+
 
 self.addEventListener("push", function(event) {
   //送られたプッシュ通知の本文を表示
