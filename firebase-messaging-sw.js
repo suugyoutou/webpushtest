0755a0909b8e5145304dea502f741942b58dc29d
Xiaodong Zou <suugyoutou@r.recruit.co.jp>
2019-05-09 19:01:59 +0900

    1

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -92,6 +92,8 @@ var messaging = firebase.messaging();
 importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js");
 
+
+
 firebase.initializeApp({
   messagingSenderId: "540875100731"
 });
