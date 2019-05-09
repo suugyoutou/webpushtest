5132e326d81caa9289d2f8451bdb070677321c8d
Xiaodong Zou <suugyoutou@r.recruit.co.jp>
2019-05-09 16:28:28 +0900

    1

--- serviceworker.js
+++ serviceworker.js
@@ -41,3 +41,33 @@ self.addEventListener('fetch',function(event){
     })
   );
 });
+
+
+
+self.addEventListener("push", function(event) {
+  console.log("Push Notification Recieved", event);
+  if (Notification.permission == "granted") {
+    event.waitUntil(
+      self.registration
+        .showNotification("受信しました", {
+          body: "お知らせです。",
+          icon: "iconV2.png"
+        })
+        .then(
+          function(showEvent) {},
+          function(error) {
+            console.log(error);
+          }
+        )
+    );
+  }
+});
+
+
+
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow("https://www.jalan.net")
+  );
+});
